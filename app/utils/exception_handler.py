76531d0909d3cd780ec07a8f3094d854f948a760
zeiadharidy <zeiad@thewebops.com>
2021-08-23 21:49:45 +0200

    handle method now allowed error

--- app/utils/exception_handler.py
+++ app/utils/exception_handler.py
@@ -4,17 +4,23 @@ from rest_framework.response import Response
 def custom_exception_handler(exc, context):
     handlers ={
       'ValidationError': _handle_validation_error,
-      'DoesNotExist': _record_does_not_exist
+      'DoesNotExist': _record_does_not_exist,
+      'MethodNotAllowed': _method_not_allowed
     }
     response = exception_handler(exc, context)
     exception_class = exc.__class__.__name__
+    print(exception_class)
     if exception_class in handlers:
       return handlers[exception_class](exc,context, response)
 
 
     return Response('Something went wrong', status = 422)
 
+def _method_not_allowed(exc,context,response):
+    return Response(str(exc), status = 404)
+
 def _record_does_not_exist(exc,context,response):
   return Response(str(exc), status = 404)
+
 def _handle_validation_error(exc, context, response):
   return Response(exc, status = 422)
